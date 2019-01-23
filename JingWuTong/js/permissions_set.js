730f09897d02580a899bfdb39afa6d371b1184e7
linqiang69672187 <69672187@qq.com>
2019-01-23 15:38:58 +0800

    1

---
+++ JingWuTong/js/permissions_set.js
@@ -0,0 +1,14 @@
+﻿var app = new Vue({
+    el: '#setbody',
+    data: {
+        rolename: '管理员',
+        creater:'林强',
+        remark:'备注信息'
+    }
+});
+
+$(function(){
+    $('tr.parent').click(function(){ // 获取所谓的父行
+      $(this).toggleClass("selected") .siblings('.child_'+this.id).toggle(); // 隐藏/显示所谓的子行
+    });
+});
